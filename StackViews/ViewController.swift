1aa7e7fc5513a7b42e9935a0987f42849b1d7a22
rrao24 <rao.ram5718@gmail.com>
2017-07-09 18:14:17 -0400

    Initial Commit

---
+++ StackViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViews
+//
+//  Created by Ramkumar Rao on 7/9/17.
+//  Copyright © 2017 Ramkumar Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
